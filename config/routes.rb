f7f513ad7786241681af2ac7e6a4e83a41fe24ea
Reka Kulinyi <reekaa85@gmail.com>
2017-11-18 16:08:51 +0100

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  devise_for :users
+  root to: "flats#index"
+  get "/flats/new", to: "flats#new"
+  post "flats", to: "flats#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
