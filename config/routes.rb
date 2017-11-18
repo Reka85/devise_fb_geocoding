9730fadae1a213c522bb5e5096034462e7799561
Reka Kulinyi <reekaa85@gmail.com>
2017-11-18 19:53:37 +0100

    fb signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: "flats#index"
   get "/flats/new", to: "flats#new"
   post "flats", to: "flats#create"
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
